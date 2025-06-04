335d6ed19c5b371c0381cf4d2696d52208633af1
Umer Farooq <umerfarooq2821035@gmail.com>
2025-06-04 20:27:36 +0500

    hosting app with firebase successfull

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJTEnfN7PykSZJoW0ePv3LR1WQLeZ2zts",
+  authDomain: "portfolio-a77d3.firebaseapp.com",
+  projectId: "portfolio-a77d3",
+  storageBucket: "portfolio-a77d3.firebasestorage.app",
+  messagingSenderId: "754168681896",
+  appId: "1:754168681896:web:3866a08edb9d5452b6799d",
+  measurementId: "G-6FSY78M0G4",
+};
+
+const app = initializeApp(firebaseConfig);
